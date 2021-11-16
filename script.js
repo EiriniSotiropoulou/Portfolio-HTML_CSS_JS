b877bedda282ffe21ea174f3dc04053e48795d6d
EiriniSotiropoulou <eirhnhsotiropoulou@gmail.com>
2021-11-16 17:14:15 +0200

    added carousel, pie and fixed responsiveness

--- script.js
+++ script.js
@@ -18,4 +18,41 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
+
 });
+
+
+
+var skillsCanvas = document.getElementById("skillsChart");
+
+// Chart.defaults.global.defaultFontFamily = "Lato";
+// Chart.defaults.global.defaultFontSize = 18;
+
+var skillData = {
+    labels: [
+        "HTML",
+        "Javascript",
+        "CSS",
+        "Java",
+        "MS Server"
+        
+    ],
+    datasets: [
+        {
+            data: [30, 20, 30, 40,40],
+            backgroundColor: [
+                "#FF6384",
+                "#63FF84",
+                "#84FF63",
+                "#8463FF",
+                "#6384FF"
+            ]
+        }]
+};
+
+var pieChart = new Chart(skillsCanvas, {
+  type: 'pie',
+  data: skillData
+});
+
+
